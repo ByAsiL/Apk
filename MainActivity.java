aa03b43dd249cf4727f06a4e85c5bdcefd8b50f9
ByAsiL <oguzhanasilkaragoz@gmail.com>
2026-05-29 04:50:32 +0300

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -5,46 +5,31 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
-import android.view.WindowInsets;
-import android.view.WindowInsetsController;
 import android.view.WindowManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.FrameLayout;
 
 public class MainActivity extends Activity {
 
     private WebView webView;
-    private FrameLayout rootLayout;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        fixSystemBars();
-
-        rootLayout = new FrameLayout(this);
-        rootLayout.setBackgroundColor(Color.parseColor("#eef2f7"));
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.parseColor("#0a1628"));
+        window.setNavigationBarColor(Color.parseColor("#334155"));
 
         webView = new WebView(this);
         webView.setBackgroundColor(Color.parseColor("#eef2f7"));
 
-        rootLayout.addView(
-                webView,
-                new FrameLayout.LayoutParams(
-                        FrameLayout.LayoutParams.MATCH_PARENT,
-                        FrameLayout.LayoutParams.MATCH_PARENT
-                )
-        );
-
-        setContentView(rootLayout);
-
-        applySafeAreaPadding();
+        setContentView(webView);
 
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
@@ -67,90 +52,6 @@ public class MainActivity extends Activity {
         webView.setWebChromeClient(new WebChromeClient());
 
         webView.loadUrl("file:///android_asset/www/index.html");
-    }
-
-    private void fixSystemBars() {
-        Window window = getWindow();
-
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-
-        window.setStatusBarColor(Color.parseColor("#0a1628"));
-        window.setNavigationBarColor(Color.parseColor("#334155"));
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.setDecorFitsSystemWindows(false);
-
-            WindowInsetsController controller = window.getInsetsController();
-            if (controller != null) {
-                controller.setSystemBarsAppearance(
-                        0,
-                        WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
-                                | WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
-                );
-            }
-        } else {
-            window.getDecorView().setSystemUiVisibility(0);
-        }
-    }
-
-    private void applySafeAreaPadding() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            rootLayout.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
-                @Override
-                public WindowInsets onApplyWindowInsets(View view, WindowInsets insets) {
-                    android.graphics.Insets bars =
-                            insets.getInsets(WindowInsets.Type.systemBars());
-
-                    view.setPadding(
-                            bars.left,
-                            bars.top,
-                            bars.right,
-                            bars.bottom
-                    );
-
-                    return insets;
-                }
-            });
-        } else {
-            rootLayout.setPadding(
-                    0,
-                    getStatusBarHeight(),
-                    0,
-                    getNavigationBarHeight()
-            );
-        }
-    }
-
-    private int getStatusBarHeight() {
-        int result = 0;
-        int resourceId = getResources().getIdentifier(
-                "status_bar_height",
-                "dimen",
-                "android"
-        );
-
-        if (resourceId > 0) {
-            result = getResources().getDimensionPixelSize(resourceId);
-        }
-
-        return result;
-    }
-
-    private int getNavigationBarHeight() {
-        int result = 0;
-        int resourceId = getResources().getIdentifier(
-                "navigation_bar_height",
-                "dimen",
-                "android"
-        );
-
-        if (resourceId > 0) {
-            result = getResources().getDimensionPixelSize(resourceId);
-        }
-
-        return result;
     }
 
     @Override
